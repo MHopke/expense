1d268785d045bb3b6e696d88e35e0bfdd61d22d2
Michael Hopke <Xemenas@Michaels-MBP.CardinalBroadband>
2015-09-18 13:04:57 -0600

    Basic CSV export added

--- Assets/Scripts/Pages/ReportPage.cs
+++ Assets/Scripts/Pages/ReportPage.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -27,6 +29,9 @@ public class ReportPage : UIView
 
 	#region Private Vars
 	double _grandTotal, _billableTotal, _reimbursementTotal;
+
+	Project _project;
+	List<Expense> _expenses;
 	#endregion
 
 	#region Overriden Methods
@@ -45,7 +50,7 @@ public class ReportPage : UIView
 	#region UI Methods
 	public void SaveToCSV()
 	{
-
+		UniFileBrowser.use.OpenFolderWindow(false,FolderSelected);
 	}
 	#endregion
 
@@ -54,6 +59,8 @@ public class ReportPage : UIView
 	{
 		if(ProjectName.text != project.Name)
 		{
+			_project = project;
+
 			List.ClearElements();
 
 			ProjectName.text = project.Name;
@@ -66,6 +73,7 @@ public class ReportPage : UIView
 
 	void ProcessExpenses(IEnumerable<Expense> expenses)
 	{
+		_expenses = new List<Expense>();
 		//Debug.Log(expenses.co
 		foreach(Expense expense in expenses)
 			AddExpense(expense);
@@ -77,6 +85,8 @@ public class ReportPage : UIView
 	}
 	void AddExpense(Expense expense)
 	{
+		_expenses.Add(expense);
+
 		ReportItem element = (ReportItem)
 			GameObject.Instantiate(Prefab,Vector3.zero,Quaternion.identity);
 		
@@ -91,6 +101,32 @@ public class ReportPage : UIView
 			_reimbursementTotal += expense.Value;
 		
 		List.AddListElement(element);
+	}
+	#endregion
+
+	#region EventListeners
+	void FolderSelected(string path)
+	{
+		string fullPath = Path.Combine(path,_project.Name + ".csv");
+
+		FileStream file = new FileStream(fullPath,FileMode.Create);
+
+		using(StreamWriter writer = new StreamWriter(file))
+		{
+			//write the headers
+			writer.WriteLine("ObjectId,Expense Item,Value");
+
+			Expense expense = null;
+			for(int index = 0; index < _expenses.Count; index++)
+			{
+				expense = _expenses[index];
+				writer.WriteLine(expense.ObjectId+","+expense.Name+","+expense.Value);
+			}
+
+			writer.WriteLine("Grand Total,," + _grandTotal);
+			writer.WriteLine("Billable Total,," + _billableTotal);
+			writer.WriteLine("Reimbursement Total,," + _reimbursementTotal);
+		}
 	}
 	#endregion
 }
